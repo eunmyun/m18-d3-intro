feae82e0ec844db8d7beede68d594a85cb0384f1
eunmyun <eunmyun@uw.edu>
2016-11-29 10:18:02 -0800

    hello

--- exercise-1/js/main.js
+++ exercise-1/js/main.js
@@ -4,12 +4,22 @@
 $(function() {
 
   // Select the element with id `sandbox` and append a `div` element to it. Make sure to store the div in a JavaScript varaible
-
+  var newDiv = $('<div>');
+  d3.select("#sandbox").append(newDiv);
   // Create a variable `mySvg` by appending an `svg` element to your newly created `div`
-
-  // Set both the width and height attributes of your `svg` to 300 
-
+  var mySvg = $('<svg>');
+  d3.newDiv.append(mySvg);
+  // Set both the width and height attributes of your `svg` to 300
+  d3.mySvg.attr('width', '300');
+  d3.mySvg.attr('height', '300');
   // Append a `circle` element to your `svg`, setting the properties noted in index.html
+  var myCircle = $('<circle>');
+  d3.myCircle.attr('r','30');
+  d3.myCircle.attr('cx','75');
+  d3.myCircle.attr('cy','30');
+  d3.myCircle.style('opacity', '0.5');
+  d3.myCircle.style('fill','blue');
+  d3.mySvg.append(myCircle);
 
   // Append a `rect` element to your `svg`, setting the properties noted in index.html
 
--- exercise-2/js/main.js
+++ exercise-2/js/main.js
@@ -15,15 +15,21 @@ var people2 = [
 // You'll have to wait for you page to load to assign events to the elements created in your index.html file
 $(function() {
   // Create a varaible myDiv by selecting the element with id my-div (this is already created for you).
-
+  var myDiv = d3.select('#my-div');
 
   // Create a variable `paragraphs` by selecting all  `p` elements in  `myDiv`, and binding you data  `people` to it.
   // Make sure to specify the `name` attribute as an identifying characteristic in your data-join.
-
+  var paragraphs = myDiv.selectAll('p')
+                        .data(people, function(d) {
+                            return d.name
+                        });
 
 
   // Add a paragraph to the div for each person in your dataset following instructions in the index.html file
-
+  paragraphs.enter()
+            .append('p');
+            .merge(paragraphs)
+            .innerHTML(paragraphs.name)
 
   // Use `setTimeout` function to wait 3 seconds, then do the following
   setTimeout(function(){
@@ -32,6 +38,6 @@ $(function() {
 
 
     // Determine which people are no longer represented in the data (`exit`), and transition their opacity to 0 and remove them.
-    
+
   }, 3000);
 });
